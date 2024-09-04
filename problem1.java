b05d805ea3aa9dc400a8ed7120e9a4a08456a0d4
kuuu111 <kritschel101@student.hbuhsd.edu>
2024-09-04 04:18:39 +0000

    finish

--- problem1.java
+++ problem1.java
@@ -1,12 +1,12 @@
 public class problem1 {
     public static void main(String[] args) {
         String name = "Messi goat";
-        
+
         int screenWidth = 80;
-        
+
         int nameLength = name.length();
         int spacesBefore = (screenWidth - nameLength) / 2;
-        
+
         String spaces = " ".repeat(spacesBefore);
 
         System.out.println(spaces + name);
