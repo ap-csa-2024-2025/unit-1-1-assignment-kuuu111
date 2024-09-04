bc8ea41c8a7e92e270dabfa46b7c869570bbd0dd
kuuu111 <kritschel101@student.hbuhsd.edu>
2024-09-04 04:13:41 +0000

    work

--- problem3.java
+++ problem3.java
@@ -1,1 +1,8 @@
-
+public class problem1 {
+    public static void main(String[] args) {
+        System.out.println("=========");
+        System.out.println("= 0   0 =   Java is Awesome!");
+        System.out.println("=   v   =");
+        System.out.println("=========");
+    }
+}
